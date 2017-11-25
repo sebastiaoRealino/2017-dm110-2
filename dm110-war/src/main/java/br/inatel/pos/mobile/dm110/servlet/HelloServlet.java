cfe520a33d7189b422ede8fdc3e832baadf9a198
Marcio Azevedo <mecva.mecva@gmail.com>
2017-11-25 14:09:56 -0200

    Creating the HelloWorld servlet.

---
+++ dm110-war/src/main/java/br/inatel/pos/mobile/dm110/servlet/HelloServlet.java
@@ -0,0 +1,24 @@
+package br.inatel.pos.mobile.dm110.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/helloservlet")
+public class HelloServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 9044518941643353335L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		PrintWriter out = resp.getWriter();
+		out.println("<h1>Hello World!!!</h1>");
+	}
+
+}
